66977c285ebe01fda7c7dbbcb1418536d7f2596e
UmasankarR <uma.sankar430@gmail.com>
2026-01-15 20:40:38 -0500

    Used global error handler

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -9,9 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddPresentation();
-
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
